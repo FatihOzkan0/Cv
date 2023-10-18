a213e72fcb2a32323141b51ea8f8893d46b4c911
FATIH <fthokn57@icloud.com>
2023-10-18 18:32:00 +0300

    .

--- Cv/App_Start/RouteConfig.cs
+++ Cv/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Cv
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "VitrinPanel", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
